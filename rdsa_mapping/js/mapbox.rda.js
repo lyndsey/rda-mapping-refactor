0ebf2e2bdeaec4d868f5a925c157f435d28e01a5
ok_lyndsey <jackson.lyndsey@gmail.com>
2025-06-09 20:14:14 +0930

    Update mapbox.rda.js
    Changing becuase the page number was hardcoded in

--- rdsa_mapping/js/mapbox.rda.js
+++ rdsa_mapping/js/mapbox.rda.js
@@ -1,41 +1,48 @@
-    mapboxgl.accessToken = 'pk.eyJ1IjoicmRhc2EiLCJhIjoiY2xrN2x5emNoMDhhZjNnbzQ2azE5cjBiZiJ9.GnFSisdnaSWxWWTjVPYjmA';
-    const map = new mapboxgl.Map({
-      container: 'mapbox-301',
-      // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-          style: 'mapbox://styles/mapbox/satellite-v9', // style URL
-          center: [-91.874, 42.76], // starting position [lng, lat]
-          zoom: 12 // starting zoom
-        });
-        console.log("here");
-        console.log(map);
-        const draw = new MapboxDraw({
-          displayControlsDefault: false,
-// Select which mapbox-gl-draw control buttons to add to the map.
-          controls: {
-            polygon: true,
-            trash: true
-          },
-// Set mapbox-gl-draw to draw by default.
-// The user does not have to click the polygon control button first.
-          defaultMode: 'draw_polygon'
-        });
-        map.addControl(draw);
+mapboxgl.accessToken = 'pk.eyJ1IjoicmRhc2EiLCJhIjoiY2xrN2x5emNoMDhhZjNnbzQ2azE5cjBiZiJ9.GnFSisdnaSWxWWTjVPYjmA';
 
-        map.on('draw.create', updateArea);
-        map.on('draw.delete', updateArea);
-        map.on('draw.update', updateArea);
+// Find a container by class or known ID
+const container = document.querySelector('.mapbox-map') || document.getElementById('map');
 
-        function updateArea(e) {
-          const data = draw.getAll();
-          const answer = document.getElementById('calculated-area');
-          if (data.features.length > 0) {
-            const area = turf.area(data);
-// Restrict the area to 2 decimal points.
-            const rounded_area = Math.round(area * 100) / 100;
-            answer.innerHTML = `<p><strong>${rounded_area}</strong></p><p>square meters</p>`;
-          } else {
-            answer.innerHTML = '';
-            if (e.type !== 'draw.delete')
-              alert('Click the map to draw a polygon.');
-          }
+if (container) {
+  const map = new mapboxgl.Map({
+    container: container,
+    style: 'mapbox://styles/mapbox/satellite-v9',
+    center: [-91.874, 42.76],
+    zoom: 12
+  });
+
+  console.log("here");
+  console.log(map);
+
+  const draw = new MapboxDraw({
+    displayControlsDefault: false,
+    controls: {
+      polygon: true,
+      trash: true
+    },
+    defaultMode: 'draw_polygon'
+  });
+
+  map.addControl(draw);
+
+  map.on('draw.create', updateArea);
+  map.on('draw.delete', updateArea);
+  map.on('draw.update', updateArea);
+
+  function updateArea(e) {
+    const data = draw.getAll();
+    const answer = document.getElementById('calculated-area');
+    if (data.features.length > 0) {
+      const area = turf.area(data);
+      const rounded_area = Math.round(area * 100) / 100;
+      answer.innerHTML = `<p><strong>${rounded_area}</strong></p><p>square meters</p>`;
+    } else {
+      answer.innerHTML = '';
+      if (e.type !== 'draw.delete') {
+        alert('Click the map to draw a polygon.');
+      }
+    }
   }
+} else {
+  console.warn('Map container not found.');
+}
